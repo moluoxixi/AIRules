f9411a8c9a92b7078b745366131d21b039ed5819
wl <1983531544@qq.com>
2026-04-21 13:41:52 +0800

    feat: initialize ESLint and Husky pre-commit hooks for project linting

---
+++ eslint.config.ts
@@ -0,0 +1,17 @@
+import eslintConfig from '@antfu/eslint-config'
+
+export default eslintConfig({
+  ignores: [
+    'scripts',
+    'public',
+    '.zed',
+  ],
+  rules: {
+    // user
+    'unused-imports/no-unused-vars': 'off',
+    'node/prefer-global/process': 'off',
+    'no-unused-vars': 'off',
+    'no-console': 'off',
+    'regexp/no-unused-capturing-group': 'off',
+  },
+})
